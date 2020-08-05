0a91f7c1a301d3a2a9133d7d5191f9de7692d8a4
Elicia Au Duong <eliciaauduong@outlook.com>
2020-08-05 20:57:30 +1000

    Updates

---
+++ cb1608f2-1e48204ef8120c529924.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{wHSu:function(c,n,o){"use strict";o.d(n,"a",(function(){return e}));var e={prefix:"fas",iconName:"chevron-left",icon:[320,512,[],"f053","M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z"]}}}]);
+//# sourceMappingURL=cb1608f2-1e48204ef8120c529924.js.map
